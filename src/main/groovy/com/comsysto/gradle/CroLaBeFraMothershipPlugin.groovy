db9fde1e30cd287bfbc127d165c081b73aa7dcea
bensteinert <benjamin.steinert@googlemail.com>
2016-01-17 14:15:25 +0100

    Finishes crolabefra task and integrates with crolabefra lang drone tasks

--- src/main/groovy/com/comsysto/gradle/CroLaBeFraMothershipPlugin.groovy
+++ src/main/groovy/com/comsysto/gradle/CroLaBeFraMothershipPlugin.groovy
@@ -1,10 +1,11 @@
-package com.comsysto.gradle;
+package com.comsysto.gradle
 
 import org.apache.tools.ant.filters.ReplaceTokens
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.FileTree
 import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Delete
 
 class CroLaBeFraMothershipPlugin implements Plugin<Project> {
 
@@ -13,40 +14,60 @@ class CroLaBeFraMothershipPlugin implements Plugin<Project> {
 
         project.tasks.create(
                 [
-                        name     : 'crolabefra',
-                        dependsOn: 'crolabefra',
-                        type     : Copy
+                        name: 'clean',
+                        type: Delete
                 ],
                 {
-                    outputs.dir "$buildDir/results"
-                    from project.resources.text.fromArchiveEntry(
-                            project.buildscript.configurations.classpath.findAll {
-                                it.name.contains 'crolabefra-mothership'
-                            },
-                            'mothership'
-                    ).asFile()
-                    into "$buildDir/results"
-
-                    doFirst {
-                        def FileTree jsonFiles = fileTree("$buildDir/results/data").include('*.js')
-                        def dataScripts = '';
-                        jsonFiles.each { file ->
-                            println file.name
-                            dataScripts += "<script src='./data/${file.name}' type='text/javascript'></script>\n"
-                        }
-                        //TODO: Hm quite heavy pattern matching ... affects all files. Probably split index.html transfer out.
-                        filter(ReplaceTokens, tokens: [crolabefraData: dataScripts])
-                    }
+                    delete project.buildDir
                 }
         )
 
+        def crolabefra = project.tasks.create(
+                [
+                        name: 'crolabefra',
+                        group: 'crolabefra',
+                        dependsOn: {
+                            def result = [];
+                            project.allprojects {
+                                if(it.plugins.findPlugin('com.comsysto.gradle.crolabefra.cpp')) {
+                                    println("Found C++ Drone Plugin ...")
+                                    result.add(":${it.name}:runCppBenchmarks")
+                                }
+                                if(it.plugins.findPlugin('com.comsysto.gradle.crolabefra.java')) {
+                                    println("Found Java Drone Plugin ...")
+                                    result.add(":${it.name}:runJavaBenchmarks")
+                                }
+                            }
 
-        task runBenchmarks(dependsOn: []) {
+                            return result;
+                        },
+                        type: Copy
+                ],
+                {
+                    def toDir = new File(project.buildDir, "/results")
+                    outputs.upToDateWhen { false }
+                    from({
+                        project.zipTree(project.buildscript.configurations.classpath.find { jars ->
+                            jars.name.contains 'crolabefra-mothership'
+                        }
+                        ).getAsFileTree().matching {
+                            include 'mothership/**'
+                        }
+                    })
+                    into toDir
+                }
+        )
+
+        crolabefra.doFirst {
+            def FileTree jsonFiles = project.fileTree(new File(project.buildDir, "/results/mothership/data")).include('*.js')
+            def dataScripts = '';
+            jsonFiles.each { file ->
+                println file.name
+                dataScripts += "<script src='./data/${file.name}' type='text/javascript'></script>\n"
+            }
+            //TODO: Hm quite heavy pattern matching ... affects all files. Probably split index.html transfer out.
+            filter(ReplaceTokens, tokens: [crolabefraData: dataScripts])
         }
-
-        task mapResults(dependsOn: []) {
-        }
-
 
     }
 }
